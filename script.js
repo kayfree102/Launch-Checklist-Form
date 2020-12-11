49182fd9e911d2c211aa60aa6ec7baec2359ef6e
Kendal Freeman <kendalmfreeman@gmail.com>
2020-12-11 17:19:58 -0600

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Write your JavaScript code here!
+//Validation
+if(inputs.pilotNameInput==="" || inputs.copilotNameInput==="" || inputs.fuelLevelInput===""
+      || inputs.cargoMassInput===""){
+            fieldsVerify();
+      }
+      else if(!isNaN(Number(inputs.pilotNameInput)) 
+      || !isNaN(Number(inputs.copilotNameInput)) 
+      || isNaN(Number(inputs.fuelLevelInput))
+      || isNaN(Number(inputs.cargoMassInput))){
+         alert("Please enter valid input.");
+         event.preventDefault();
+}
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
